0e16271af08060ec67343c632af4f3b8c9e58aff
Olli Kanerva <olli.kanerva@aalto.fi>
2017-09-03 15:29:37 +0300

    new build

--- src/_environment.js
+++ src/_environment.js
@@ -1,6 +1,6 @@
 export default (config) => ({
   local:'localhost:8080/',
-  dev : 'http://tietokilta.fi/scivoo',
+  dev : 'http://tenttiarkisto.fi/scivoo',
   initialRoute: '/',
   courseRoute: 'course/:id',
   loginRoute: '/login',
