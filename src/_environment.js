1d1f759edb6bbc7355cb0038b2409ac06f97ddb9
Olli Kanerva <olli.kanerva@aalto.fi>
2017-11-13 21:49:29 +0200

    fix

--- src/_environment.js
+++ src/_environment.js
@@ -1,6 +1,6 @@
 export default (config) => ({
   local:'localhost:8080/',
-  dev : 'http://varjoopintoopas.fi/',
+  dev : '/',
   initialRoute: '/',
   courseRoute: 'course/:id',
   loginRoute: '/login',
